4fbb93d821d46fb8f3658a79167a060fde68e768
Vova <vhavrikov@gmail.com>
2021-01-29 21:03:00 +0200

    add modal window

--- app/js/select.js
+++ app/js/select.js
@@ -1,6 +1,8 @@
 'use strict';
 
 
+
+
 /// book form, select
 const selectSingle = document.querySelector('.__select');
 const selectSingle_title = selectSingle.querySelector('.__select__title');
@@ -30,15 +32,7 @@ let inputName = document.getElementById("name");
 let inputSurname = document.getElementById("surname");
 let inputEmail = document.getElementById("email");
 let inputPhone = document.getElementById("phone");
-
-
-
-// var a = {
-// 	'BOOL': true,
-// 	'INT': 20,
-// 	'ARRAY': [1, 2, 3],
-// 	'STRING': 'Тише, мыши, кот на крыше'
-//  };
+// let selectTour = document.querySelector('__select__title');
 
 
 
@@ -47,5 +41,50 @@ buttonBook.addEventListener('click', function () {
 	localStorage.setItem("surname", inputSurname.value);
 	localStorage.setItem("email", inputEmail.value);
 	localStorage.setItem("phone", inputPhone.value);
+	// localStorage.setItem("Chosed tour", selectTour.value);
+
+
+
+	let modal_name = document.createElement('p');
+	let modal_surname = document.createElement('p');
+	let modal_email = document.createElement('p');
+	let modal_phone = document.createElement('p');
+
+
+
+
+	// if (inputName && inputSurname && inputEmail && inputPhone) {
+
+
+		modal_name.id = 'modal_name';
+		modal_surname.id = 'modal_surname';
+		modal_email.id = 'modal_email';
+		modal_phone.id = 'modal_phone';
+
+		modal_name.className = 'info';
+		modal_surname.className = 'info';
+		modal_email.className = 'info';
+		modal_phone.className = 'info';
+
+
+		document.querySelector('.my_modal-body').appendChild(modal_name);
+		document.querySelector('.my_modal-body').appendChild(modal_surname);
+		document.querySelector('.my_modal-body').appendChild(modal_email);
+		document.querySelector('.my_modal-body').appendChild(modal_phone);
+
+
+		document.getElementById('modal_name').innerHTML = "Name:   " + inputName.value;
+		document.getElementById('modal_surname').innerHTML = "Surname:   " + inputSurname.value;
+		document.getElementById('modal_email').innerHTML = "Email:   " + inputEmail.value;
+		document.getElementById('modal_phone').innerHTML = "Number phone:   " + inputPhone.value;
+
+	// }
+
+
+
+
+
+
+
 
 });
